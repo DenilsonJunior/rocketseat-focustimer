9a8ff22f111feb23c4ee6033d607cfed144df882
Denilson Junior <dehjr1990@gmail.com>
2023-06-29 00:20:07 -0300

    [REFACTURE]

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
 // default import
 import Controls from "./controls.js"
 import Timer from "./timer.js"
-
-const buttonPlay = document.querySelector('.play')
-const buttonPause = document.querySelector('.pause')
-const buttonStop = document.querySelector('.stop')
-const buttonSet = document.querySelector('.set')
-const buttonSoundOn = document.querySelector('.sound-on')
-const buttonSoundOff = document.querySelector('.sound-off')
-const minutesDisplay = document.querySelector('.minutes')
-const secondsDisplay = document.querySelector('.seconds')
+import Events from "./events.js"
+import Sound from "./sounds.js"
+import { 
+    buttonPlay,
+    buttonPause,
+    buttonStop,
+    buttonSet,
+    minutesDisplay,
+    secondsDisplay, 
+} from "./elements.js"
 
 const controls = Controls({
     buttonPlay,
@@ -24,39 +25,6 @@ const timer = Timer({
     resetControls: controls.reset,
 })
 
-buttonPlay.addEventListener('click', function() {
-    controls.play()
-    timer.countdown()
-})
+const sound = Sound();
 
-buttonPause.addEventListener('click', function() {
-    controls.pause()
-    timer.hold()
-})
-
-buttonStop.addEventListener('click', function() {
-    controls.reset()
-    timer.reset()
-})
-
-buttonSoundOff.addEventListener('click', function() {
-    buttonSoundOn.classList.remove('hide')
-    buttonSoundOff.classList.add('hide')
-})
-
-buttonSoundOn.addEventListener('click', function() {
-    buttonSoundOn.classList.add('hide')
-    buttonSoundOff.classList.remove('hide')
-})
-
-buttonSet.addEventListener('click', function() {
-    let newMinutes = controls.getMinutes()
-
-    if (!newMinutes) {
-        timer.reset()
-        return
-    }
-
-    timer.updateDisplay(newMinutes, 0)
-    timer.updateMinutes(newMinutes)
-})
+Events({controls, timer, sound})
--- js/timer.js
+++ js/timer.js
@@ -1,13 +1,13 @@
+import Sounds from "./sounds.js";
+
 export default function Timer({ 
-        minutesDisplay, 
-        secondsDisplay, 
-        resetControls
-    }) {
-  
+    minutesDisplay, 
+    secondsDisplay, 
+    resetControls
+}) {
     let timerTimeOut
     let minutes = Number(minutesDisplay.textContent)
-    let isFinished = minutes <= 0 && seconds <= 0
-  
+    
     function updateDisplay(newMinutes, seconds) {
         newMinutes = newMinutes === undefined ? minutes : newMinutes;
         seconds = seconds === undefined ? 0 : seconds;
@@ -22,19 +22,21 @@ export default function Timer({
   
     function countdown(){
         timerTimeOut = setTimeout(function() {
-            let seconds =  Number(secondsDisplay.textContent);
             let minutes = Number(minutesDisplay.textContent);
+            let seconds =  Number(secondsDisplay.textContent);
+            let isFinished = minutes <= 0 && seconds <= 0;
     
             updateDisplay(minutes, 0)
-    
+
             if ( isFinished ) {
                 resetControls();
                 updateDisplay();
+                Sounds().timeEnd();
                 return;
             }
     
             if( seconds <= 0 ) {
-                seconds = 60;
+                seconds = 3;
                 --minutes;
             }
     
